d0a01fb943143783dfbfc393eb5c076a030a5171
reachscript-jak <123ja.k123@gmail.com>
2021-11-25 20:19:23 +0900

    chore

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>じゃけぇです</p>
+        <p>更新した！！！！！</p>
 
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
